7d8e26bb62c93ca780ab06c7fb38963133251ff1
Stéphane Sénési <senesi@posteo.net>
2022-07-26 05:59:20 +0200

    stage list_module.sh

---
+++ liste_modules.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# List packages to be included in a conda environment
+
+# This file is sources by install_all.sh, and provides a value for $mdoules
+# Its current content matches the needs dor C-ESM-EP
+# S.Sénési - 07/2022
+
+# Make the list of modules, organized by component :
+# CliMAF, notebooks, C-ESM-EP, ESMValTool
+
+# CliMAF
+climaf_modules="natsort ujson xarray netcdf4 cftime yaml pyyaml sphinx"
+climaf_exec="cdo<2.0.4 ncl imagemagick ncview nco!=5.0.4 exiv2 perl ipython pipenv "
+# Note: CliMAF also needs pdftk, but there is no conda package for that, only a Ubuntu package
+
+# Notebooks
+nb_modules="jupyter jupytext papermill texlive-core"
+
+# C-ESM-EP
+cesmep_modules="cdms2 cdutil "
+general_modules="dask joblib dask-jobqueue windspharm pandas regionmask geopandas gdal matplotlib  basemap"
+r_modules="r-irkernel r-evd r-ncdf4 r-foreach r-doParallel r-goftest Cython"
+
+#ESMValTool
+evt_modules="esmvaltool iris"
+
+# Aggregates all modules
+modules="$climaf_modules $climaf_exec $cesmep_modules $general_modules $nb_modules $evt_modules $r_modules "
+
